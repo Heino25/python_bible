83544f3f6089a49f984380b43556e9857ade624c
Heino <heinoduplessis@gmail.com>
2021-06-04 16:50:58 +0200

    Project 1 part1

---
+++ Chapter4/Health.py
@@ -0,0 +1,10 @@
+#project 1
+import random
+
+#Giving a player health
+health = 50
+
+#creating a health potion generating a random amount of health 
+health_potion = random.randint(25,50)
+
+print(health_potion)
---
+++ Chapter4/Ordering_Operations.py
@@ -0,0 +1,9 @@
+ # Beackets ()
+ # Order
+ # Division
+ # Multiplication
+ # Addition
+ # Subtraction
+ 
+print(2*5-1)
+print(2*(5-1))
