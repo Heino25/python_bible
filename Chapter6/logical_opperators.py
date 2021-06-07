bac652c64280ca1c0f4c3e97d56ca9851aeeb70d
Heino <heinoduplessis@gmail.com>
2021-06-07 12:59:49 +0200

    logical opperators

---
+++ Chapter6/logical_opperators.py
@@ -0,0 +1,15 @@
+print(not True)
+print(not False)
+print(not 2<3)
+print(not 2>3)
+print(not 4==3)
+
+x = 10
+y = 20
+if not y < x:
+	print("It worked")
+
+C = 10
+D = 5
+if C >= 10 and D > 1:
+	print("it worked again")
