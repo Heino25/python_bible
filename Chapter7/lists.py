c70c6020c0dd185d58916214c006d4e64e63022b
Heino <heinoduplessis@gmail.com>
2021-06-08 08:32:36 +0200

    travis security system

---
+++ Chapter7/lists.py
@@ -0,0 +1,13 @@
+#Creating a list
+our_list = [27, 46, -5, 17, 99]
+print(our_list)
+jackson = ["A", "B", "C", 1, 2, 3, "Do", "Rey", "Mi", True, False]
+print(jackson[4])
+print(jackson[-3])
+x = jackson[6]
+print(x)
+#Slicing a list
+print(jackson[0:3])
+#Slicing a list inside a list
+our_list = [1, 2, [3, 4, 5] , 6, 7, 8]
+print(our_list[2])
