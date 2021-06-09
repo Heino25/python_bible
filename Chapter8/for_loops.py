0bc0f8d9693b6019c4d094f221a058a3ff745219
Heino <heinoduplessis@gmail.com>
2021-06-09 13:11:10 +0200

    dictionary used with for loops

--- Chapter8/for_loops.py
+++ Chapter8/for_loops.py
@@ -1,21 +1,31 @@
-#range saves the effort of typing 1 to 1001
+# #range saves the effort of typing 1 to 1001
 # for number in range(1,1001):
 # 	print(number)
 
 # for number in range(1,11,2):
 # 	print(number)
 
-vowels = 1
-consonants = 0
+# vowels = 1
+# consonants = 0
 
-for letter in "supercalifragilisticexpialidocious":
-	if letter.lower() in "aeiou":
-		vowels = vowels + 1
-	elif letter == "":
-		pass
-	else:
-		consonants = consonants + 1
+# for letter in "supercalifragilisticexpialidocious":
+# 	if letter.lower() in "aeiou":
+# 		vowels = vowels + 1
+# 	elif letter == "":
+# 		pass
+# 	else:
+# 		consonants = consonants + 1
 
 
-print("There are {} vowels".format(vowels))
-print("There are {} consonants".format(consonants))
+# print("There are {} vowels".format(vowels))
+# print("There are {} consonants".format(consonants))
+
+students = {
+	"male":["Tom", "Charli", "Harry", "Frank"],
+	"female":["Sara", "Britney", "Huda", "Samantha"]
+}
+
+for key in students.keys():
+	for name in students[key]:
+		if "a" in name:
+			print(name)
