403cf2a45cb545cda2da5183361187be012ddbb6
Heino <heinoduplessis@gmail.com>
2021-06-09 14:02:59 +0200

    pig latin

--- Chapter8/for_loops.py
+++ Chapter8/for_loops.py
@@ -1,24 +1,24 @@
-# #range saves the effort of typing 1 to 1001
-# for number in range(1,1001):
-# 	print(number)
+#range saves the effort of typing 1 to 1001
+for number in range(1,1001):
+	print(number)
 
-# for number in range(1,11,2):
-# 	print(number)
+for number in range(1,11,2):
+	print(number)
 
-# vowels = 1
-# consonants = 0
+vowels = 1
+consonants = 0
 
-# for letter in "supercalifragilisticexpialidocious":
-# 	if letter.lower() in "aeiou":
-# 		vowels = vowels + 1
-# 	elif letter == "":
-# 		pass
-# 	else:
-# 		consonants = consonants + 1
+for letter in "supercalifragilisticexpialidocious":
+	if letter.lower() in "aeiou":
+		vowels = vowels + 1
+	elif letter == "":
+		pass
+	else:
+		consonants = consonants + 1
 
 
-# print("There are {} vowels".format(vowels))
-# print("There are {} consonants".format(consonants))
+print("There are {} vowels".format(vowels))
+print("There are {} consonants".format(consonants))
 
 students = {
 	"male":["Tom", "Charli", "Harry", "Frank"],
